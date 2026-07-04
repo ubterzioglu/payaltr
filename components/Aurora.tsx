3c0e4f76e7f572647b27d58f59a4d43aa3395a9e
ubterzioglu <corteqssocial@gmail.com>
2026-07-04 23:49:23 +0200

    feat: add animated aurora background

---
+++ components/Aurora.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Aurora() {
+  return (
+    <div className="aurora" aria-hidden="true">
+      <div className="aurora__blob aurora__blob--a" />
+      <div className="aurora__blob aurora__blob--b" />
+      <div className="aurora__blob aurora__blob--c" />
+    </div>
+  );
+}
